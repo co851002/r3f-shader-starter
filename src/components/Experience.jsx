6cb8038e39bffd84b68fc61dd3ca3d7129018660
chris oosthuizen <co851002@gmail.com>
2024-01-18 12:33:59 +0000

    first commit

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,12 +1,37 @@
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, shaderMaterial, useTexture} from "@react-three/drei";
+import { extend, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+
+export const WaveShaderMaterial = shaderMaterial(
+  //Uniform
+  {
+    uColor: new THREE.Color(0.0,0.0,0.0),
+  },
+  //Vertex
+  `
+    void main() {
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+  //Fragment
+  `
+    uniform vec3 uColor;
+    void main() {
+      gl_FragColor = vec4(uColor,0.8);
+    }
+  `,
+);
+
+extend({WaveShaderMaterial});
 
 export const Experience = () => {
   return (
     <>
       <OrbitControls />
+      <pointLight position={[10,10,10]}/>
       <mesh>
-        <boxGeometry />
-        <meshNormalMaterial />
+        <planeBufferGeometry args={[3,5]}/>
+        <waveShaderMaterial uColor={"hotpink"}/>
       </mesh>
     </>
   );
