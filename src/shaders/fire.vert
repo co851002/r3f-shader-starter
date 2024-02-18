3753c2c3828e2628247aca5d2b6c874341c7f2c9
chris oosthuizen <co851002@gmail.com>
2024-02-18 23:06:00 +0000

    -a

---
+++ src/shaders/fire.vert
@@ -0,0 +1,8 @@
+// fire.vert
+uniform mat4 projectionMatrix;
+uniform mat4 modelViewMatrix;
+attribute vec3 position;
+
+void main() {
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import glsl from "vite-plugin-glsl";
+import {glslify} from 'vite-plugin-glslify';
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    glslify(),
+    glsl({
+      include: /\.(glsl|vs|fs|vert|frag)$/,
+      exclude: /node_modules/,
+      compress: false,
+      glslify: true // Enable glslify support
+    })
+  ],
+});
